e4d552a587920793fa24d4027f73ea06477e8030
matt-colen <mzcolen@gmail.com>
2023-10-02 16:03:59 -0500

    Refactored all files
    Major refactor of all files to clean up code.

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
 // DOM ELEMENTS
 const convertBtn = document.querySelector("#convert-btn");
-const inputEl = document.querySelector("#input-el");
+const inputEl = document.querySelector("#number-input");
 
-const convertUnits = (el) => {
-  const numberToConvert = el.value;
-  const conversions = [
-    3.2808, 0.3048, 0.2642, 3.7854, 2.2046, 0.45359999999999995,
-  ];
-  const convertedArr = conversions.map((conversion) => {
-    let result = conversion * numberToConvert;
-    result = result.toFixed(3);
-    return result;
-  });
-  render(convertedArr, numberToConvert);
+// CONVERSIONS
+const conversions = {
+  METERS_TO_FEET: 3.2808,
+  FEET_TO_METERS: 0.3048,
+  LITERS_TO_GALLONS: 0.2642,
+  GALLONS_TO_LITERS: 3.7854,
+  KILOS_TO_POUNDS: 2.2046,
+  POUNDS_TO_KILOS: 0.45359999999999995,
 };
 
-const render = (arr, num) => {
-  const LengthEl = document.querySelector("#results-length");
-  const volumeEl = document.querySelector("#results-volume");
-  const massEl = document.querySelector("#results-mass");
-  LengthEl.textContent = `${num} meters = ${arr[0]} feet | ${num} feet = ${arr[1]} meters`;
-  volumeEl.textContent = `${num} liters = ${arr[2]} gallons | ${num} gallons = ${arr[3]} liters`;
-  massEl.textContent = `${num} kilos = ${arr[4]} pounds | ${num} pounds = ${arr[5]} kilos`;
+// Convert Units Function
+const convertUnits = (el) => {
+  const numberToConvert = +el.value; // Num value of the el
+  const convertedArr = []; // Arr to store the conversions
+
+  // Loops through the conversions obj and pushes the conversion to the arr
+  for (let key in conversions) {
+    let result = numberToConvert * conversions[key];
+    result = result.toFixed(3);
+    convertedArr.push(result);
+  }
+
+  // Passes the conversion arr and original number to the render function
+  renderResults(convertedArr, numberToConvert);
+};
+
+// Render Results Function
+const renderResults = (arr, num) => {
+  const resultCards = document.querySelectorAll(".results__card");
+  // Stores the different template literals being used 
+  const resultText = [
+    `${num} meters = ${arr[0]} feet | ${num} feet = ${arr[1]} meters`,
+    `${num} liters = ${arr[2]} gallons | ${num} gallons = ${arr[3]} liters`,
+    `${num} kilos = ${arr[4]} pounds | ${num} pounds = ${arr[5]} kilos`,
+  ];
+
+  // Updates the textContent for each card & triggers the animation
+  resultCards.forEach((card, index) => {
+    card.textContent = resultText[index];
+    triggerAnimation(card);
+  });
+};
+
+// Trigger Animation Function
+const triggerAnimation = (card) => {
+  card.classList.add("results__card-ani");
+  card.addEventListener("animationend", () => {
+    card.classList.remove("results__card-ani");
+    card.style.display = "flex";
+  });
 };
 
 // EVENT LISTENERS
@@ -31,8 +61,8 @@ convertBtn.addEventListener("click", () => {
 
 //
 inputEl.addEventListener("focus", () => {
-  const label = document.querySelector("#hanging-label");
-  label.classList.add("hanging-label--float");
+  const label = document.querySelector("#number-label");
+  label.classList.add("number-label--float");
 });
 
 inputEl.addEventListener("keyup", (e) => {
